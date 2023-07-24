3fa4da91b7ba4ab2aafaf67af0ee5a0023163980
Mohammad <mmh.ksx.2002@gmail.com>
2023-07-24 20:32:42 +0330

    Make Toggle And Delet Func and Fix Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   const [todos, setTodos] = useState([])
   const [paginatedTodos, setPaginatedTodos] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
+  const [removeTodo, setRemoveTodo] = useState(false)
 
   let pageSize = 10;
   let pageNumbers;
@@ -13,21 +14,22 @@ export default function App() {
       .then((res) => res.json())
       .then((data) => {
         setTodos(data)
-        let endIndex = currentPage * pageSize;
-        let startIndex = endIndex - pageSize;
-        let allShownTodos = data.slice(startIndex, endIndex);
-        setPaginatedTodos(allShownTodos);
+        updatePaginate(data)
       })
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
+    updatePaginate(todos)
+  }, [currentPage, removeTodo])
+
+  const updatePaginate = (datas) => {
     let endIndex = currentPage * pageSize;
     let startIndex = endIndex - pageSize;
-    let allShownTodos = todos.slice(startIndex, endIndex);
+    let allShownTodos = datas.slice(startIndex, endIndex);
     setPaginatedTodos(allShownTodos);
-  }, [currentPage])
-  
+  }
+
   let pagesCount = Math.ceil(todos.length / pageSize)
   pageNumbers = Array.from(Array(pagesCount).keys())
 
@@ -35,9 +37,24 @@ export default function App() {
     setCurrentPage(newPage)
   }
 
+  const toggleHandler = (id) => {
+    let newTodos = [...todos]
+    let todoIndex = newTodos.findIndex((todo) => todo.id === id)
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed
+    setTodos(newTodos)
+  }
+
+  const removeHandler = (id) => {
+    let newTodos = [...todos]
+    let todoIndex = newTodos.findIndex((todo) => todo.id === id)
+    newTodos.splice(todoIndex, 1)
+    setTodos(newTodos)
+    setRemoveTodo(!removeTodo)
+  }
+
   return (
     <div className="p-2">
-      <table className="w-full border border-gray-300">
+      <table className="w-full  border-gray-300">
         <thead className="text-left bg-gray-100">
           <tr>
             <th className="border border-gray-300 p-2">ID</th>
@@ -52,9 +69,17 @@ export default function App() {
               <td className="border border-gray-300 p-2">{todo.id}</td>
               <td className="border border-gray-300 p-2">{todo.title}</td>
               <td className={`border border-gray-300 p-2 ${todo.completed ? "text-green-600" : "text-red-600"}`}>{todo.completed ? "Completed" : "Pending"}</td>
-              <td className="border border-gray-300 p-2">
-                <button>OK</button>
-                <button>Del</button>
+              <td className="border border-gray-300 p-2 text-center">
+                <button className="md:mr-3" onClick={() => { toggleHandler(todo.id) }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-green-600 transition-all">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                  </svg>
+                </button>
+                <button className="md:ml-3" onClick={() => { removeHandler(todo.id) }}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-500 hover:text-red-600 transition-all">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                  </svg>
+                </button>
               </td>
             </tr>
           ))}
